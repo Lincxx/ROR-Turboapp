e03e3554f7ea19efefe00e40dcbc7f1aeeeca350
Lincxx <bossco28@gmail.com>
2022-05-05 16:29:39 -0400

    Added turbo stream to edit and update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,6 +17,15 @@ class MessagesController < ApplicationController
 
   # GET /messages/1/edit
   def edit
+    respond_to do |format|
+      format.turbo_stream do render turbo_stream: 
+                            turbo_stream.update(
+                              @message, 
+                              partial: "messages/form", 
+                              locals: {message: @message}
+                              )
+                            end
+    end
   end
 
   # POST /messages or /messages.json
@@ -54,10 +63,26 @@ class MessagesController < ApplicationController
   # PATCH/PUT /messages/1 or /messages/1.json
   def update
     respond_to do |format|
+      
+        format.turbo_stream do render turbo_stream: 
+                              turbo_stream.update(
+                                @message, 
+                                partial: "messages/message", 
+                                locals: {message: @message}
+                                )
+                              end
+      
       if @message.update(message_params)
         format.html { redirect_to message_url(@message), notice: "Message was successfully updated." }
         format.json { render :show, status: :ok, location: @message }
       else
+        format.turbo_stream do render turbo_stream: 
+          turbo_stream.update(
+            @message, 
+            partial: "messages/form", 
+            locals: {message: @message}
+            )
+          end
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
@@ -67,8 +92,9 @@ class MessagesController < ApplicationController
   # DELETE /messages/1 or /messages/1.json
   def destroy
     @message.destroy
-
+    puts "fired"
     respond_to do |format|
+      format.turbo_stream  {render turbo_stream: turbo_stream.remove(@message)}
       format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
       format.json { head :no_content }
     end
